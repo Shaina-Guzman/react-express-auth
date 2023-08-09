9e2ad799976817a01fc709541c5941f8b652b797
Shaina <shainaguzman0624@gmail.com>
2023-08-09 15:34:04 -0400

    migration and modeles completed

--- src/middleware/add-models-to-request.js
+++ src/middleware/add-models-to-request.js
@@ -1,8 +1,14 @@
 const User = require('../db/models/user');
+const Quizzes = require('../db/models/quizzes');
+const Quiz_Attempts = require('../db/models/quiz_attempts');
+const Quiz_Questions = require('../db/models/quiz_questions');
 
 const addModelsToRequest = (req, res, next) => {
   req.db = {
     User,
+    Quizzes,
+    Quiz_Attempts,
+    Quiz_Questions
   };
   next();
 };
